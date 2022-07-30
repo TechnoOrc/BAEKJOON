ab69eb0d1630d0715f02c57bf0a69abdbc2f4fbb
TechnoOrc <technoorc@gmail.com>
2022-07-30 23:55:19 +0900

    기초 연습문제 풀이

---
+++ BAEKJOON/src/step1/No10172.java
@@ -0,0 +1,26 @@
+package step1;
+
+/*
+ * 문제
+ * 아래 예제와 같이 개를 출력하시오.
+ * 
+ * 예제 출력 1
+ *|\_/|
+ *|q p|   /}
+ *( 0 )"""\
+ *|"^"`    |
+ *||_/=\\__|
+ */
+
+public class No10172 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}//main
+
+}//class
