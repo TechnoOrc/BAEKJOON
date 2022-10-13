ff9426f7ba2a968a661c3881de0de0c7e697f203
TechnoOrc <technoorc@gmail.com>
2022-10-14 00:19:26 +0900

    test message

---
+++ BAEKJOON/src/step1/Test.java
@@ -0,0 +1,11 @@
+package step1;
+
+public class Test {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+
+}
