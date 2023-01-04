698305ade9f169fa35b213f22ae9927baee5b4ca
TechnoOrc <technoorc@gmail.com>
2023-01-04 12:34:22 +0900

    백준 10950문제

---
+++ src/step2/No10950.java
@@ -0,0 +1,23 @@
+package step2;
+
+import java.util.Scanner;
+
+public class No10950 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a,b,t = 0;
+
+        t = sc.nextInt();
+
+        for(int i = 0; i < t ; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a + b);
+        }
+        sc.close();
+        
+
+    }//main
+}//class
