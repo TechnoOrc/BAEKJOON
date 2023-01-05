eb6fc0434fb6b4f710f499ad0b0effb5d2e8172f
TechnoOrc <technoorc@gmail.com>
2023-01-05 12:46:42 +0900

    백준 2530 문제

---
+++ src/step2/No15552.java
@@ -0,0 +1,9 @@
+package step2;
+
+public class No15552 {
+    public static void main(String[] args) {
+        
+        
+
+    }//main
+}//class
---
+++ src/step2/No2530.java
@@ -0,0 +1,33 @@
+package step2;
+
+import java.util.Scanner;
+
+public class No2530 {
+    public static void main(String[] args) {
+        
+        int totalPrice = 0;
+        int sumPrice = 0;
+        int n = 0;
+        int a = 0;
+        int b = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        totalPrice = sc.nextInt();
+        n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            sumPrice = ( a * b ) + sumPrice;
+        }
+            sc.close();
+
+        if(totalPrice == sumPrice){
+            System.out.print("Yes");
+        }else {
+            System.out.print("No");
+        }
+
+
+    }//main
+}//class
