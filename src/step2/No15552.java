9f2c9b0b4fd783b7766019aff33a3e3f78de57f5
TechnoOrc <technoorc@gmail.com>
2023-01-06 12:58:10 +0900

    백준 15552 문제

--- src/step2/No15552.java
+++ src/step2/No15552.java
@@ -1,8 +1,21 @@
 package step2;
 
+import java.util.Scanner;
+
 public class No15552 {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        int t = 0;
+        int a,b = 0;
+        t = sc.nextInt();
+        for(int i = 0; i < t ; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(a + b);
+            sc.close();
+        }//for
+
         
 
     }//main
