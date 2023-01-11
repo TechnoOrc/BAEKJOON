77fbd6e40c24af4b169a8e25b07b254ff67f65ee
TechnoOrc <technoorc@gmail.com>
2023-01-11 12:33:50 +0900

    백준 2438문제

--- src/step2/No2739.java
+++ src/step2/No2739.java
@@ -8,7 +8,7 @@ public class No2739 {
         
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        sc.close();
         for(int i = 1; i <= 9; i ++){
             System.out.println(n + " * " + i + " = " + n * i );
         }
