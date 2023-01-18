ed198c75bce462e99880b333669baf8d311feae8
TechnoOrc <technoorc@gmail.com>
2023-01-18 13:01:29 +0900

    백준 1110문제 도전중

--- src/step2/No1110.java
+++ src/step2/No1110.java
@@ -1,7 +1,45 @@
 package step2;
 
+import java.util.Scanner;
+
 public class No1110 {
     public static void main(String[] args) {
-        
-    }//main
-}//class
+        Scanner sc = new Scanner(System.in);
+
+        int before_a;
+        String str;
+
+        int after_a;
+        int after_b;
+
+        before_a = sc.nextInt();
+        sc.close();
+
+        if (before_a >= 0 && before_a < 99) {
+
+            str = Integer.toString(before_a);
+            System.out.println("str >>> " + str);
+            System.out.println("str.substring(0,1) >>> " + str.substring(0, 1));
+            System.out.println("str.substring(1,2) >>> " + str.substring(1, 2));
+            if ( before_a < 10 ){
+                after_a = 0;
+                after_b = Integer.parseInt(str.substring(0, 1));
+            } else {
+                after_a = Integer.parseInt(str.substring(0, 1));
+                after_b = Integer.parseInt(str.substring(1, 2));
+            }
+                
+
+            System.out.println("after_a >>> " + after_a);
+            System.out.println("after_b >>> " + after_b);
+
+            // while(before_a >= 0 && before_a < 99){
+            // for(int i = 1; before_a >= 0 && before_a < 99; i++){
+            // System.out.println("i >>> " + i);
+            // }//for
+
+            // }//while
+        } // if
+
+    }// main
+}// class
