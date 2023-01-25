ca78f38d5570839670f8f556c87d11ba62134c6b
TechnoOrc <technoorc@gmail.com>
2023-01-25 19:01:37 +0900

    백준 1110 문제

--- src/step2/No1110.java
+++ src/step2/No1110.java
@@ -7,31 +7,42 @@ public class No1110 {
         Scanner sc = new Scanner(System.in);
 
         int before_a;
-        String str;
+        String before_str;
 
         int after_a;
         int after_b;
+        int after_sum = 0;
+        String after_str;
+
+
 
         before_a = sc.nextInt();
         sc.close();
 
         if (before_a >= 0 && before_a < 99) {
 
-            str = Integer.toString(before_a);
-            System.out.println("str >>> " + str);
-            System.out.println("str.substring(0,1) >>> " + str.substring(0, 1));
-            System.out.println("str.substring(1,2) >>> " + str.substring(1, 2));
+            before_str = Integer.toString(before_a); // 숫자를 문자로
+            System.out.println("before_str >>> " + before_str);
+            System.out.println("before_str.substring(0,1) >>> " + before_str.substring(0, 1)); // 문자열를 자르기
             if ( before_a < 10 ){
                 after_a = 0;
-                after_b = Integer.parseInt(str.substring(0, 1));
+                after_b = Integer.parseInt(before_str.substring(0, 1)); // 문자를 숫자로
+                after_sum = after_a + after_b;
             } else {
-                after_a = Integer.parseInt(str.substring(0, 1));
-                after_b = Integer.parseInt(str.substring(1, 2));
+                after_a = Integer.parseInt(before_str.substring(0, 1)); // 문자를 숫자로
+                after_b = Integer.parseInt(before_str.substring(1, 2)); // 문자를 숫자로 
+                after_sum = after_a + after_b;
             }
-                
-
+            after_str = Integer.toString(after_b) + Integer.toString(after_sum); // 숫자를 문자로
             System.out.println("after_a >>> " + after_a);
             System.out.println("after_b >>> " + after_b);
+            System.out.println("after_str >>> " + after_str);
+            System.out.println("after_sum >>> " + after_sum);
+
+            
+            
+
+
 
             // while(before_a >= 0 && before_a < 99){
             // for(int i = 1; before_a >= 0 && before_a < 99; i++){
